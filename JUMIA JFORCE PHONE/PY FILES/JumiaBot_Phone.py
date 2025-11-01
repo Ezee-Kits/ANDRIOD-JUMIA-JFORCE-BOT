9e592d13c528626c9dcdbd67fb848782439907ca
Ezee Kits <ezeekits@gmail.com>
2025-11-01 11:19:31 +0100

    Update JumiaBot_Phone.py
    Added retry logic for loading product CSV file and improved error handling for each social media bot interaction.

--- JUMIA JFORCE PHONE/PY FILES/JumiaBot_Phone.py
+++ JUMIA JFORCE PHONE/PY FILES/JumiaBot_Phone.py
@@ -50,53 +50,69 @@ async def main():
     }
     input(' \n ENTER AFTER SETTING UP YOUR ACCOUNT PROPERLY \n :::::::::::') # USE ONLY ONCE
 
+
     for all_runings in range(1,100):
         if all_runings % 5 == 0:
             Getting_urls()
 
         main_func_random()
-        product_csv_path = [os.path.join('PRODUCT', file_name) for file_name in os.listdir('PRODUCT') if file_name.endswith('csv') ][0]
-        product_df = pd.read_csv(product_csv_path)
+
+        for retrying in range(5):
+            try:
+                product_csv_path = [os.path.join('PRODUCT', file_name) for file_name in os.listdir('PRODUCT') if file_name.endswith('csv') ][0]
+                product_df = pd.read_csv(product_csv_path)
+                if product_df:
+                    break
+            except:
+                print('\n ERROR OCCURED IN THE RUNNING OF THE CSV PRODUCT FILE AND RETRYING {retrying}\n')
+
+
+        try:
+            try:
+                await tabs["Facebook"].close()
+            except Exception as e:
+                print("⚠️ Could not close old Facebook tab:", e)
+            tabs["Facebook"] = await browser.newPage()
+            await tabs["Facebook"].goto("https://web.facebook.com/", {"timeout": 0,"waitUntil": "networkidle2"})
+            await Facebook_Bot(page=tabs["Facebook"], df=product_df)
+        except:
+            print('\n ERROR HAPPENDED ON Facebook \n')
         
         try:
-            await tabs["Facebook"].close()
-        except Exception as e:
-            print("⚠️ Could not close old Facebook tab:", e)
-        tabs["Facebook"] = await browser.newPage()
-        await tabs["Facebook"].goto("https://web.facebook.com/", {"timeout": 0,"waitUntil": "networkidle2"})
-        await Facebook_Bot(page=tabs["Facebook"], df=product_df)
-
-
-        
-        try:
-            await tabs["Facebook_MarketPlace"].close()
-        except Exception as e:
-            print("⚠️ Could not close old Facebook_MarketPlace tab:", e)
-        tabs["Facebook_MarketPlace"] = await browser.newPage()
-        await tabs["Facebook_MarketPlace"].goto("https://web.facebook.com/groups/361814259953483", {"timeout": 0,"waitUntil": "networkidle2"})
-        await Facebook_MarketPlace_Bot(page=tabs["Facebook_MarketPlace"], df=product_df)
-
+            try:
+                await tabs["Facebook_MarketPlace"].close()
+            except Exception as e:
+                print("⚠️ Could not close old Facebook_MarketPlace tab:", e)
+            tabs["Facebook_MarketPlace"] = await browser.newPage()
+            await tabs["Facebook_MarketPlace"].goto("https://web.facebook.com/groups/361814259953483", {"timeout": 0,"waitUntil": "networkidle2"})
+            await Facebook_MarketPlace_Bot(page=tabs["Facebook_MarketPlace"], df=product_df)
+        except:
+            print('\n ERROR HAPPENDED ON Facebook_MarketPlace \n')
 
         try:
-            await tabs["Instagram"].close()
-        except Exception as e:
-            print("⚠️ Could not close old Instagram tab:", e)
-        tabs["Instagram"] = await browser.newPage()
-        await tabs["Instagram"].goto("https://www.instagram.com/", {"timeout": 0,"waitUntil": "networkidle2"})
-        await Instagram_Bot(page=tabs["Instagram"], df=product_df)
+            try:
+                await tabs["Instagram"].close()
+            except Exception as e:
+                print("⚠️ Could not close old Instagram tab:", e)
+            tabs["Instagram"] = await browser.newPage()
+            await tabs["Instagram"].goto("https://www.instagram.com/", {"timeout": 0,"waitUntil": "networkidle2"})
+            await Instagram_Bot(page=tabs["Instagram"], df=product_df)
+        except:
+            print('\n ERROR HAPPENDED ON Instagram \n')
 
 
         try:
-            await tabs["Twitter"].close()
-        except Exception as e:
-            print("⚠️ Could not close old Twitter tab:", e)
-        tabs["Twitter"] = await browser.newPage()
-        await tabs["Twitter"].goto("https://x.com/home", {"timeout": 0,"waitUntil": "networkidle2"})
-        await Twitter_Bot(page=tabs["Twitter"], df=product_df)
-
+            try:
+                await tabs["Twitter"].close()
+            except Exception as e:
+                print("⚠️ Could not close old Twitter tab:", e)
+            tabs["Twitter"] = await browser.newPage()
+            await tabs["Twitter"].goto("https://x.com/home", {"timeout": 0,"waitUntil": "networkidle2"})
+            await Twitter_Bot(page=tabs["Twitter"], df=product_df)
+        except:
+            print('\n ERROR HAPPENDED ON Twitter \n')
+            
         await asyncio.sleep(7)  # Wait for 7 seconds before the next iteration
- 
-
 
     await browser.close()
 
